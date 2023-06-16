21725b953376b01d2959f8fe5049b9ebf6cd03c8
Atakan Demir <demiratakan2002@gmail.com>
2023-06-16 14:27:57 +0300

    Atakan Demir
    Online Sınav Otomasyon Sistemi (Online Exam Automation System)

---
+++ ososAPI/ViewModel/SoruModel.cs
@@ -0,0 +1,17 @@
+﻿using ososAPİ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ososAPİ.ViewModel
+{
+    public class SoruModel
+    {
+        public int soruId { get; set; }
+        public string soruText { get; set; }
+        public int soruSinavId { get; set; }
+
+        public List<SecenekModel> secenekler { get; set; }
+    }
+}
